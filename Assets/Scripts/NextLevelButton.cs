2260aabda54d5c6414f74433407716b44f3e2f3c
SeongHyeon Ji <j.eq.h0507@gmail.com>
2024-07-15 10:52:01 +0900

    feat : 다음 레벨 이동 활성화 여부 조건 추가

--- Assets/Scripts/NextLevelButton.cs
+++ Assets/Scripts/NextLevelButton.cs
@@ -14,21 +14,18 @@ public class NextLevelButton : MonoBehaviour
     private void Awake()
     {
         nextLevelButton = GetComponent<Button>();
-        // 게임 스테이지 수와 클리어 수를 비교해서 활성화 여부 결정하기
     }
 
     private void Start()
     {
         nextLevel = GetNextLevelFromSceneName();
+        SetButtonInteractable();
     }
 
     public void OnButtonClick()
     {
         // 버튼 클릭음 재생
         AudioManager.Instance.SfxPlayer.PlaySfx(SfxType.Click);
-
-        // 게임 매니저의 클리어 스테이지 카운트 개수 올리기 (이건 트레저 스크립트에 넣는게 더 알맞아 보임)
-
 
         LoadingSceneManager.Instance.ChangeScene("Scenes/Level " + nextLevel);
     }
@@ -41,5 +38,12 @@ public class NextLevelButton : MonoBehaviour
 
         // currentSceneName.Last()은 number char이므로 '0'의 아스키 값을 빼주면 int 숫자 타입을 얻을 수 있음
         return currentSceneName.Last() - '0' + 1;
+    }
+
+    private void SetButtonInteractable()
+    {
+        bool isLastStage =
+            GameManager.Instance.TotalLevelCount == GameManager.Instance.ClearedStages;
+        nextLevelButton.interactable = !isLastStage;
     }
 }
